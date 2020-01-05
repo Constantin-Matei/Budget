fcb9301dd1d9d9de6a28c7eb5fc7b123d06e5788
Constantin-Matei <53446122+Constantin-Matei@users.noreply.github.com>
2020-01-05 18:33:08 +0200

    Add some code...

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 // ! Budget Controller
 var budgetController = (function() {
-  // Some code
+		var Expense = function(id, description, value) {
+			this.id = id;
+			this.description = description;
+			this.value = value;
+		};
+
+		var Income = function(id, description, value) {
+			this.id = id;
+			this.description = description;
+			this.value = value;
+		};
+
+		var data = {
+			allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp = 0,
+			inc = 0
+		},
+
+	};
+	
+	return {
+		addItem: function(type, des, val) {
+			var newItem;
+			if (type === 'exp') {
+						newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			} // Aici m-am oprit :)
+			
+
+		}
+	};
+
+
 })();
 
 // ! UI CONTROLLER
